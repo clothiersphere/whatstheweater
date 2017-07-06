f5ffb6b30fd6e9b319de4e516d725ec04d92c486
ChristopherLee <xchrislee@gmail.com>
2017-07-06 16:20:03 -0700

    Accuweather properly loading png through file loader

--- src/components/SetWeather/DarkskyFeed.js
+++ src/components/SetWeather/DarkskyFeed.js
@@ -1,4 +1,8 @@
 import React from 'react';
+// import Skycons from 'react-skycons';
+import Skycons from 'skycons';
+
+
 
 var DarkskyFeed = function(props) {
   if (props.weather[0]) {
@@ -8,7 +12,9 @@ var DarkskyFeed = function(props) {
         {
           props.weather[0].map((weather, key) => {
             return <div className="dskyInfo" key={key}> 
-              TEMP: HIGH: {weather.temperatureMax}, LOW: {weather.temperatureMin}
+              <canvas id={key} width='128' height='128'></canvas>
+              CONDITIONS: {weather.summary} <Skycons color='white' icon={weather.icon.toUpperCase().replace(/-/g, '_')}/>
+              HIGH: {weather.temperatureMax}, LOW: {weather.temperatureMin}
               PRECIP: {weather.precipProbability}
               HUMID: {weather.humidity * 100}
             </div>
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
       test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'react-hot-loader!babel-loader'
+    },
+    { test: /\.(png|jpg|gif)$/, 
+      loader: 'file-loader?name=./images/[name].[ext]' 
     }]
   },
   resolve: {
